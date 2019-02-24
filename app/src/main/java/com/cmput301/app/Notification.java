239baa1afa35ade977340e1656005e2eccf25f77
Vinny1230 <baijoo@ualberta.ca>
2019-02-23 18:04:44 -0800

    Notification

---
+++ app/src/main/java/com/cmput301/app/Notification.java
@@ -0,0 +1,18 @@
+package com.cmput301.app;
+
+public class Notification {
+    private String notifDesc;
+
+
+    public Notification(String notifDesc) {
+        this.notifDesc = notifDesc;
+    }
+
+    public String getNotifDesc() {
+        return notifDesc;
+    }
+
+    public void setNotifDesc(String notifDesc) {
+        this.notifDesc = notifDesc;
+    }
+}
