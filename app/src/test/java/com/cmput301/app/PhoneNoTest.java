b26320d1e0ee337ebb8a3d495fd960e7565255f9
Vinny1230 <baijoo@ualberta.ca>
2019-02-24 18:29:09 -0800

    PhoneTest

--- app/src/test/java/com/cmput301/app/PhoneNoTest.java
+++ app/src/test/java/com/cmput301/app/PhoneNoTest.java
@@ -12,4 +12,22 @@ public class PhoneNoTest {
         assertEquals(aPhone.getFirstDigits(),(Integer) 456);
         assertEquals(aPhone.getLastDigits(), (Integer) 4555);
     }
+
+    public void testToString() {
+        PhoneNo aPhone = new PhoneNo(123,456,4555);
+        assertEquals(aPhone.toString(), "123-456-4555");
+
+    }
+
+    public void testSetPhoneNo() {
+        PhoneNo aPhone = new PhoneNo(123,456,4555);
+        aPhone.setPhoneNo(555,192,1234);
+        assertEquals(aPhone.getFirstDigits(),(Integer) 192);
+        assertEquals(aPhone.getAreaCode(), (Integer) 123);
+        assertEquals(aPhone.getLastDigits(), (Integer) 4555);
+    }
+
+    
+
+
 }
