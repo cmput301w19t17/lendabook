d72d2458d01c1d1cc96ae6389ab19c1e755088fb
Oleg Krushelnytskyy <oleg@ualberta.ca>
2019-03-06 22:36:56 -0700

    Update UserInformation.java

--- lendABook/app/src/main/java/com/example/android/lendabook/LogIn/UserInformation.java
+++ lendABook/app/src/main/java/com/example/android/lendabook/LogIn/UserInformation.java
@@ -1,2 +1,22 @@
 package com.example.android.lendabook.LogIn;
 
+public class UserInformation {
+
+    public String username;
+    public String full_name;
+    public String website;
+    public String description;
+    public String phone_number;
+
+    public UserInformation(){
+
+    }
+
+    public UserInformation(String username, String full_name, String website, String description, String phone_number) {
+        this.username = username;
+        this.full_name = full_name;
+        this.website = website;
+        this.description = description;
+        this.phone_number = phone_number;
+    }
+}
