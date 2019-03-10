608dc23e86232c711b81c2b8cd5b38f218a7014a
Abenezer Belachew <belachew@ualberta.ca>
2019-03-09 17:47:33 -0700

    User can now choose picture from gallery to upload and remove as he/she wishes to

--- lendABook/app/src/main/java/com/example/android/lendabook/Add/AddActivity.java
+++ lendABook/app/src/main/java/com/example/android/lendabook/Add/AddActivity.java
@@ -1,20 +1,34 @@
 package com.example.android.lendabook.Add;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.lendabook.R;
 import com.example.android.lendabook.Utils.BottomNavigationViewHelper;
 import com.example.android.lendabook.Utils.fetchData;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static com.example.android.lendabook.Utils.fetchData.isbn;
 import static com.example.android.lendabook.Utils.fetchData.parsedAuthor;
@@ -25,17 +39,44 @@ public class AddActivity extends AppCompatActivity {
     private static final String TAG = "AddActivity";
     private Context mContext = AddActivity.this;
     private static final int ACTIVITY_NUM = 2;
+    private static final int RESULT_LOAD_IMAGE = 1;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         Log.d(TAG, "onCreate: started");
 
+        TextView include_picture = findViewById(R.id.include_picture_text_view);
+        final ImageView remove_image = findViewById(R.id.remove_image);
+        include_picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getPhoto();
+            }
+        });
+
+        remove_image.setVisibility(View.GONE);
+
         setupToolbar();
         setUpBottomNavigationView();
         fillTextView();
     }
 
+
+    private void getPhoto() {
+        // open image picker
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(chooserIntent, RESULT_LOAD_IMAGE);
+    }
     // fills text boxes with data from book api
     private void fillTextView(){
         EditText isbnTitle = (EditText) findViewById(R.id.input_book_title);
@@ -69,6 +110,37 @@ public class AddActivity extends AppCompatActivity {
     private void setupToolbar() {
         Toolbar toolbar =  findViewById(R.id.profileToolBar);
         setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        final ImageView selected_image = findViewById(R.id.selected_image);
+        final ImageView remove_image = findViewById(R.id.remove_image);
+        remove_image.setVisibility(View.VISIBLE);
+
+        remove_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selected_image.setImageBitmap(null);
+                remove_image.setVisibility(View.GONE);
+            }
+        });
+
+        if(resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case RESULT_LOAD_IMAGE:
+                    Uri selectedImage = data.getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                        selected_image.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        Log.i("TAG", "Some exception " + e);
+                    }
+                    break;
+            }
 
     }
 }
