cdc77694c37c17bb38774366dee82cd6737f0de8
Oleg Krushelnytskyy <oleg@ualberta.ca>
2019-03-11 14:14:09 -0600

    Update Notification1.java

--- lendABook/app/src/main/java/com/example/android/lendabook/Notification1.java
+++ lendABook/app/src/main/java/com/example/android/lendabook/Notification1.java
@@ -1,9 +1,22 @@
 package com.example.android.lendabook;
 
+/**
+ * Created by kostin on 2019-03-04.
+ * Class for notifications.
+ */
+
+/**
+ * The class for info in notifications.
+ */
+
 public class Notification1 {
     private String notifDesc;
-
-
+    
+    /**
+     * creates object of notification with string notifDesc
+     *
+     * @param notifDesc
+     */
     public Notification1(String notifDesc) {
         this.notifDesc = notifDesc;
     }
