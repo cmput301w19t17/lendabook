78397ae5dedefc8662c03943d703405bfe3d9e8f
Hwarinho <oleg@ualberta.ca>
2019-03-11 12:33:40 -0600

    all

--- lendABook/app/src/main/java/com/example/android/lendabook/LogIn/RegisterActivity.java
+++ lendABook/app/src/main/java/com/example/android/lendabook/LogIn/RegisterActivity.java
@@ -21,17 +21,31 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+/**
+ * Created by oleg on 2019-03-03.
+ * Class for Registering a new user.
+ */
 
+/**
+ * The Register Activity for when creating a profile
+ *  Class uses Firebase database to authenticate a new user and then set the information for the user to the real time database.
+ *  You need a username, full name, email, password to make the initial registration.
+ *  Additional info saved in real time database for user includes phone number, website and description.
+ */
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "RegisterActivity";
+
+    //define firebase authentication and reference.
     private FirebaseAuth Authentication;
+    private DatabaseReference mRef;
 
     // define all editText and button
     private EditText input_username;
@@ -40,16 +54,26 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText input_password;
     private Button register;
 
-    private DatabaseReference mRef;
+    // static int that is used to keep track of if the username is in use
+    public static boolean username_taken;
 
+    /**
+     * Sets up and initializes the Activity.
+     * Waits for click to start the SaveToRegisterDB method.
+     *
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Log.d(TAG, "onCreate: started");
 
-        Authentication = FirebaseAuth.getInstance();
+        //set the initial username taken as false
+        RegisterActivity.username_taken = false;
 
+        //get instance of firebase for authentication and real time database
+        Authentication = FirebaseAuth.getInstance();
         mRef = FirebaseDatabase.getInstance().getReference();
 
         // link all buttons/editText
@@ -59,6 +83,8 @@ public class RegisterActivity extends AppCompatActivity {
         input_password = (EditText) findViewById(R.id.input_password);
         register = (Button) findViewById(R.id.btn_register);
 
+        //OnClickListener for the register button. This will authenticate a new account as well as adding info about the account
+        //to the real time database.
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,6 +93,12 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * On Firebase, authenticates an account and saves info pertaining to the account to the realtime database.
+     * Checks if any entries are empty, if they are, do not proceed with account creation.
+     * Also checks if username if already registered for an account. (not working properly yet)
+     * After account creation, redirects the user to the Log In page.
+     */
     public void saveToRegisterDB(){
         String username = input_username.getText().toString();
         String email = input_email.getText().toString();
@@ -93,7 +125,6 @@ public class RegisterActivity extends AppCompatActivity {
             return;
         }
 
-
         //Checks if the username entered is unique
         // found at: https://stackoverflow.com/questions/38948905/how-can-i-check-if-a-value-exists-already-in-a-firebase-data-class-android/41646772
         FirebaseUser user = Authentication.getCurrentUser();
@@ -102,8 +133,8 @@ public class RegisterActivity extends AppCompatActivity {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         if(dataSnapshot.exists()){
+                            RegisterActivity.username_taken =true;
                             Toast.makeText(RegisterActivity.this, "Username Taken.",Toast.LENGTH_SHORT).show();
-                        }else{
                         }
                     }
                     @Override
@@ -112,27 +143,35 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 });
 
-        Authentication.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            String username = input_username.getText().toString();
-                            String full_name = input_full_name.getText().toString();
+        //if the username is taken, we can not make a new account in the db with this username.
+        if(!RegisterActivity.username_taken){
+            //Authenticates a user account with email and password and if the authentication is successful, it will save username and full name to real time database.
+            Authentication.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                String username = input_username.getText().toString();
+                                String full_name = input_full_name.getText().toString();
 
-                            UserInformation userInfo = new UserInformation(username, full_name, "none", "none", "none");
+                                //creates a new object with the information provided as well as future info provided and stores it for the uid of the authenticated user
+                                //in real time database.
+                                UserInformation userInfo = new UserInformation(username, full_name, "", "", "");
 
-                            FirebaseUser user = Authentication.getCurrentUser();
-                            mRef.child("Users").child(user.getUid()).setValue(userInfo);
+                                FirebaseUser user = Authentication.getCurrentUser();
+                                mRef.child("Users").child(user.getUid()).setValue(userInfo);
 
-                            Toast.makeText(RegisterActivity.this, "Registration Successful.",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(RegisterActivity.this, "Registration Successful.",Toast.LENGTH_SHORT).show();
 
-                            Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
-                            startActivity(intent);
-                        }else{
-                            Toast.makeText(RegisterActivity.this, "Registration Failed, please try again!",Toast.LENGTH_SHORT).show();
+                                //Registration complete so moves the user to the login page
+                                Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(RegisterActivity.this, "Registration Failed, please try again!",Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+        }
+        RegisterActivity.username_taken = false;
     }
 }
--- lendABook/app/src/main/java/com/example/android/lendabook/Profile/ProfileActivity.java
+++ lendABook/app/src/main/java/com/example/android/lendabook/Profile/ProfileActivity.java
@@ -3,8 +3,10 @@ package com.example.android.lendabook.Profile;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,40 +14,101 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.android.lendabook.R;
 import com.example.android.lendabook.Utils.BottomNavigationViewHelper;
 import com.example.android.lendabook.Utils.UniversalImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+/**
+ * Created by belachew, oleg on 2019-03-03.
+ * belachew: initial setup along with UI
+ * oleg: pulling info from database and setting info in the User Profile.
+ * Class for displaying the user profile.
+ */
 
+/**
+ * The ProfileActivity allows user to see his own profile.
+ *  Class uses Firebase database to pull information about the user from real time database and display it.
+ *  The information populates the TextView's in the layout.
+ */
 public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "ProfileActivity";
+    //Sets context
     private Context mContext = ProfileActivity.this;
     private static final int ACTIVITY_NUM = 4;
 
+    //for firebase
+    private FirebaseAuth Authentication;
+    private DatabaseReference mRef;
+
+    //define progressBar and default profile photo (we only use default for now)
     private ProgressBar mProgressBar;
     private ImageView profilePhoto;
 
+    /**
+     * Sets a layout, define the database information, sets up what will be displayed on the page.
+     *
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started");
 
+        //for firebase.
+        mRef = FirebaseDatabase.getInstance().getReference();
+        Authentication = FirebaseAuth.getInstance();
 
+        //set up what will be displayed on the profile page.
         setUpBottomNavigationView();
         setupToolbar();
         setupActivityWidget();
-        setProfileImage();
+        setProfileImageAndFullName();
+        setupUserInformation();
     }
 
-    private void setProfileImage() {
+    /**
+     * Sets a default picture as well as the users full name under the picture
+     */
+    private void setProfileImageAndFullName() {
         Log.d(TAG, "setProfileImage: setting profile photo.");
         String imgURL = "https://i.pinimg.com/236x/d5/7a/62/d57a62ed19396108635f8e5d50af9cb3--book-nerd-music-books.jpg";
         UniversalImageLoader.setImage(imgURL, profilePhoto, mProgressBar, "");
 
+        //Set the name under the picture to the users full name
+        FirebaseUser user = Authentication.getCurrentUser();
+
+        //Firebase query for the fullName (to make it always stay under the picture)
+        mRef = FirebaseDatabase.getInstance().getReference("Users");
+        Query query = mRef.child(user.getUid());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String FullName = (String) dataSnapshot.child("full_name").getValue().toString();
+                //Once we find the full associated with this account, we display it on the profile page.
+                TextView name = findViewById(R.id.FullName);
+                name.setText(FullName);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
     }
 
+    /**
+     * Sets up a widget for progresBar and profilePhoto
+     */
     private void setupActivityWidget() {
         mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
         mProgressBar.setVisibility(View.GONE);
@@ -53,13 +116,33 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     /**
-     * Responsible for setting up the profile toolbar
+     * Responsible for setting up the profile toolbar and username on the toolbar
      */
     private void setupToolbar() {
         Toolbar toolbar =  findViewById(R.id.profileToolBar);
         setSupportActionBar(toolbar);
 
         ImageView profileMenu = findViewById(R.id.profile_menu);
+        FirebaseUser user = Authentication.getCurrentUser();
+
+        //Firebase query for the username (to make it always stay on the top toolbar)
+        mRef = FirebaseDatabase.getInstance().getReference("Users");
+        Query query = mRef.child(user.getUid());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String username = (String) dataSnapshot.child("username").getValue().toString();
+                //Once we find the username associated with this account, we display it on the profile page.
+                TextView profileName = findViewById(R.id.profileName);
+                profileName.setText(username);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //navigates to account setting upon clicking profileMenu
         profileMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,6 +152,43 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    /**
+     * Method to set up user information of the profile page
+     */
+    private void setupUserInformation(){
+        TextView email = findViewById(R.id.email);
+        //defines current user
+        FirebaseUser user = Authentication.getInstance().getCurrentUser();
+
+        //Set the email in contact information to the email of the account
+        String user_email = user.getEmail();
+        email.setText("Email: "+user_email);
+
+        //Firebase query for email,phone number, description and website (to make it always stay under the picture)
+        mRef = FirebaseDatabase.getInstance().getReference("Users");
+        Query query = mRef.child(user.getUid());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String PhoneNo = (String) dataSnapshot.child("phone_number").getValue().toString();
+                String Website = (String) dataSnapshot.child("website").getValue().toString();
+                String aboutMeDescription = (String) dataSnapshot.child("description").getValue().toString();
+                //Once we find the phone number, website and description associated with this account, we display it on the profile page.
+                TextView phone_number = findViewById(R.id.phone_number);
+                //format the phone number
+                String formatPhoneNo = PhoneNumberUtils.formatNumber(PhoneNo);
+                phone_number.setText("Phone number: "+formatPhoneNo);
+                TextView website = findViewById(R.id.website);
+                website.setText("Website: "+Website);
+                TextView Description = findViewById(R.id.description);
+                Description.setText(aboutMeDescription);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
     }
 
     /**
@@ -83,7 +203,4 @@ public class ProfileActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
-
-
-
 }
